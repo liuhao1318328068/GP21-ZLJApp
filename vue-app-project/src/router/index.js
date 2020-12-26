b26570db0cfb689e4337e95f71b4191b3edbb32f
liuhao1318328068 <1318328068@qq.com>
2020-12-26 18:55:45 +0800

    刘浩的第二次正式上传
    完成了基本骨架，路由，框架已经搭好

---
+++ vue-app-project/src/router/index.js
@@ -0,0 +1,58 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Home from "@/views/Home";
+import Index from "@/views/home/index/Index"
+import Classify from "@/views/home/classify/Classify";
+import Sell from "@/views/home/sell/Sell"
+import Group from "@/views/home/group/Group"
+import Mine from "@/views/home/mine/Mine"
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+    redirect: "/home/index",
+    children: [
+      {
+        path: "index",
+        name:"index",
+        component: Index,
+      },
+      {
+        path: "classify",
+        name:"classify",
+        component: Classify,
+      },
+      {
+        path: "sell",
+        name:"sell",
+        component: Sell,
+      },
+      {
+        path: "group",
+        name:"group",
+        component: Group,
+      },
+      {
+        path: "mine",
+        name:"mine",
+        component: Mine,
+      },
+    ],
+  },
+];
+
+const router = new Router({
+  mode: "history",
+  routes,
+});
+
+export default router;
---
+++ vue-app-project/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrap">
+    <router-view></router-view>
+    <nav class="tabbar">
+      <Tabbar />
+    </nav>
+  </div>
+</template>
+
+<script>
+import Tabbar from "@/components/Tabbar"
+export default {
+  components: {
+    Tabbar
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/stylus/border.styl';
+.wrap 
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  .main 
+    flex: 1;
+    overflow-y scroll
+
+  .tabbar 
+    height: 0.48rem;
+    border1px(1px 0 0 0, #d7d7d7);
+    overflow hidden
+</style>
